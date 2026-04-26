8e2d8db0ec62be807a27a9a4355489622063e7a2
randomusert <eetupajamaki@gmail.com>
2026-04-26 10:31:57 +0300

    log os ver

--- src/main/java/io/github/randomusert/tinteeam/gtool/api/GToolApi.java
+++ src/main/java/io/github/randomusert/tinteeam/gtool/api/GToolApi.java
@@ -6,5 +6,10 @@ import io.github.randomusert.tinteeam.gtool.api.logging.GtoolLogger;
 public class GToolApi {
     public static void init() {
         GtoolLogger.logInfo("Gtool API init started");
+
+        String OS = System.getProperty("os.name");
+
+
+        GtoolLogger.logInfo("Running the tool on " + OS);
     }
 }
